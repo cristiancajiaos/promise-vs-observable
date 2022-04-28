3fe01a97e659f3ec94aac2833e29c481ade70c2f
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-28 14:36:10 -0400

    Promesa VS Observable: N° valores emitidos

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -10,6 +11,19 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const numberPromise = new Promise((resolve, reject) => {
+      resolve(5);
+      resolve(15);
+    });
+
+    numberPromise.then(value => console.log(`Promise: ${value}`));
+
+    const numberObservables = new Observable(observer => {
+      observer.next(5);
+      observer.next(15);
+    });
+
+    numberObservables.subscribe(value => console.log(`Subscribe: ${value}`));
   }
 
 }
